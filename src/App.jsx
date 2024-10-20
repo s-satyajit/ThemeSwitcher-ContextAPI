8a50b1d243e2f2c3159a9da16385845741051d53
Satyajit Samal <samalsatyajit668@gmail.com>
2024-10-20 23:40:39 +0530

    Created App.jsx file nad wrapped ThemeProvider in the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <ThemeProvider>
+      
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              {/* {ThemeBtn} */}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+              {/* {Card} */}
+          </div>
+        </div>
+      </div>
+
+    </ThemeProvider>
   )
 }
 
