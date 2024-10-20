6f663148eb19875d68299cdcfebc2d21bcafbe24
Satyajit Samal <samalsatyajit668@gmail.com>
2024-10-20 23:44:04 +0530

    Added value to ThemeProvider in app.jsx and defined those values

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,20 @@ import { ThemeProvider } from './context/theme'
 
 function App() {
 
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  // Logic for change in background
+
   return (
-    <ThemeProvider>
-      
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}} >
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -17,7 +28,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </ThemeProvider>
   )
 }
